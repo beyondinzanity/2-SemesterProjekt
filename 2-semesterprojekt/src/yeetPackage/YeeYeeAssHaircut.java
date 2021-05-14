d4ebc942ca89c3fa0c705ba09cedf62d8150ec8f
TheisTechWizard <75674693+TheisTechWizard@users.noreply.github.com>
2021-05-14 09:30:57 +0200

    :+1:

--- 2-semesterprojekt/src/yeetPackage/YeeYeeAssHaircut.java
+++ 2-semesterprojekt/src/yeetPackage/YeeYeeAssHaircut.java
@@ -1,5 +1,11 @@
 package yeetPackage;
 
 public class YeeYeeAssHaircut {
-
+	public static void main(String[] args) {
+		System.out.println("Ay nigga don't hate me cause I'm beautiful nigga. " + "\n"
+				+ "Maybe if you got rid of that old yee-yee ass haircut, you'd get some bitches on yo dick. Oh, better yet, "
+				+ "\n"
+				+ "maybe Tanisha'll call your dog ass if she stops fuckin' with that brain surgeon or lawyer she fucking with. "
+				+ "\n" + "Niggaaaaaaaaa");
+	}
 }
