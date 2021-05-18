bb6487d0b4e8f103dba950beddfa255b6b61646b
BeyonDInzanitY <nicklas@ostvand.dk>
2021-05-18 15:12:55 +0200

    GUI mostly complete - find a way to return to main window

--- 2-semesterprojekt/src/gui/CreateRentalPanel.java
+++ 2-semesterprojekt/src/gui/CreateRentalPanel.java
@@ -1,180 +1,172 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Button;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.List;
+import java.awt.TextField;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Button;
-import java.awt.TextField;
-import java.awt.List;
-import java.awt.Label;
-import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class CreateRental extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CreateRental frame = new CreateRental();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+public class CreateRentalPanel extends JPanel {
+	
+	
+	public void switchPanel(JPanel panel) {
+		removeAll();
+		add(panel);
+		repaint();
+		revalidate();
 	}
-
+	
 	/**
-	 * Create the frame.
+	 * Create the panel.
 	 */
-	public CreateRental() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 913, 542);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+	public CreateRentalPanel() {
+		setBounds(0, 0, 920, 550);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(null);
 		
 		Button button = new Button("S\u00F8g Hj\u00E6lpemiddel");
-		button.setBounds(27, 25, 105, 22);
-		contentPane.add(button);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//switchPanel();
+			}
+		});
+		button.setBounds(27, 25, 115, 22);
+		add(button);
 		
 		Button button_1 = new Button("S\u00F8g Beboer");
-		button_1.setBounds(141, 25, 105, 22);
-		contentPane.add(button_1);
+		button_1.setBounds(152, 25, 115, 22);
+		add(button_1);
 		
 		Button button_2 = new Button("Opret Udl\u00E5n");
-		button_2.setBounds(27, 53, 219, 22);
-		contentPane.add(button_2);
+		button_2.setBounds(27, 53, 240, 22);
+		add(button_2);
 		
 		TextField textField = new TextField();
 		textField.setBounds(27, 94, 292, 22);
-		contentPane.add(textField);
+		add(textField);
 		
 		Button button_3 = new Button("S\u00F8g");
 		button_3.setBounds(325, 94, 52, 22);
-		contentPane.add(button_3);
+		add(button_3);
 		
 		List list = new List();
 		list.setBounds(27, 122, 350, 332);
-		contentPane.add(list);
+		add(list);
 		
 		Label label = new Label("BEBOER");
-		label.setFont(new Font("Dialog", Font.PLAIN, 20));
 		label.setBounds(463, 53, 105, 22);
-		contentPane.add(label);
+		label.setFont(new Font("Dialog", Font.PLAIN, 20));
+		add(label);
 		
 		Label label_1 = new Label("UDL\u00C5N");
-		label_1.setFont(new Font("Dialog", Font.PLAIN, 20));
 		label_1.setBounds(463, 250, 105, 22);
-		contentPane.add(label_1);
+		label_1.setFont(new Font("Dialog", Font.PLAIN, 20));
+		add(label_1);
 		
 		Label label_2 = new Label("//TODO_LOGIN_INFO");
 		label_2.setBounds(758, 10, 115, 22);
-		contentPane.add(label_2);
+		add(label_2);
 		
 		Label label_3 = new Label("HJ\u00C6LPEMIDDEL");
 		label_3.setBounds(454, 291, 85, 22);
-		contentPane.add(label_3);
+		add(label_3);
 		
 		Label label_4 = new Label("START DATO");
-		label_4.setBounds(454, 319, 75, 22);
-		contentPane.add(label_4);
+		label_4.setBounds(454, 319, 85, 22);
+		add(label_4);
 		
 		Label label_5 = new Label("SLUT DATO");
-		label_5.setBounds(454, 347, 62, 22);
-		contentPane.add(label_5);
+		label_5.setBounds(454, 347, 85, 22);
+		add(label_5);
 		
 		Label label_6 = new Label("UDLEJNINGS ID");
 		label_6.setBounds(454, 375, 85, 22);
-		contentPane.add(label_6);
+		add(label_6);
 		
 		Label label_7 = new Label("NAVN");
 		label_7.setBounds(595, 83, 62, 22);
-		contentPane.add(label_7);
+		add(label_7);
 		
 		Label label_8 = new Label("CPR-NR");
 		label_8.setBounds(595, 111, 62, 22);
-		contentPane.add(label_8);
+		add(label_8);
 		
 		Label label_9 = new Label("ADRESSE");
 		label_9.setBounds(595, 139, 62, 22);
-		contentPane.add(label_9);
+		add(label_9);
 		
 		Label label_10 = new Label("BOLIG NUMMER");
 		label_10.setBounds(595, 167, 85, 22);
-		contentPane.add(label_10);
+		add(label_10);
 		
 		Label label_11 = new Label("KOMMUNE");
 		label_11.setBounds(595, 195, 62, 22);
-		contentPane.add(label_11);
+		add(label_11);
 		
 		Label label_12 = new Label("ID #");
 		label_12.setBounds(618, 224, 62, 22);
-		contentPane.add(label_12);
+		add(label_12);
 		
 		Button button_4 = new Button("Tilf\u00F8j hj\u00E6lpemiddel");
-		button_4.setBounds(454, 432, 99, 22);
-		contentPane.add(button_4);
+		button_4.setBounds(454, 432, 114, 22);
+		add(button_4);
 		
 		Button button_5 = new Button("Annuller udlejning");
-		button_5.setBounds(596, 471, 105, 22);
-		contentPane.add(button_5);
+		button_5.setBounds(596, 471, 115, 22);
+		add(button_5);
 		
 		Button button_6 = new Button("Gem Udlejning");
-		button_6.setBounds(728, 471, 105, 22);
-		contentPane.add(button_6);
+		button_6.setBounds(728, 471, 115, 22);
+		add(button_6);
 		
 		TextField textField_1 = new TextField();
 		textField_1.setBounds(545, 291, 190, 22);
-		contentPane.add(textField_1);
+		add(textField_1);
 		
 		TextField textField_2 = new TextField();
 		textField_2.setBounds(545, 319, 133, 22);
-		contentPane.add(textField_2);
+		add(textField_2);
 		
 		Label label_13 = new Label("ID");
 		label_13.setBounds(754, 291, 17, 22);
-		contentPane.add(label_13);
+		add(label_13);
 		
 		TextField textField_3 = new TextField();
 		textField_3.setBounds(777, 291, 75, 22);
-		contentPane.add(textField_3);
+		add(textField_3);
 		
 		TextField textField_4 = new TextField();
 		textField_4.setBounds(545, 347, 133, 22);
-		contentPane.add(textField_4);
+		add(textField_4);
 		
 		TextField textField_5 = new TextField();
 		textField_5.setBounds(545, 375, 133, 22);
-		contentPane.add(textField_5);
+		add(textField_5);
 		
 		TextField textField_6 = new TextField();
 		textField_6.setBounds(686, 167, 166, 22);
-		contentPane.add(textField_6);
+		add(textField_6);
 		
 		TextField textField_7 = new TextField();
 		textField_7.setBounds(686, 195, 166, 22);
-		contentPane.add(textField_7);
+		add(textField_7);
 		
 		TextField textField_8 = new TextField();
 		textField_8.setBounds(686, 83, 166, 22);
-		contentPane.add(textField_8);
+		add(textField_8);
 		
 		TextField textField_9 = new TextField();
 		textField_9.setBounds(686, 111, 166, 22);
-		contentPane.add(textField_9);
+		add(textField_9);
 		
 		TextField textField_10 = new TextField();
 		textField_10.setBounds(686, 139, 166, 22);
-		contentPane.add(textField_10);
+		add(textField_10);
+
 	}
 }
--- 2-semesterprojekt/src/gui/SearchAssistiveDevice.java
+++ 2-semesterprojekt/src/gui/SearchAssistiveDevice.java
@@ -1,7 +1,14 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.List;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,6 +17,7 @@ import javax.swing.border.EmptyBorder;
 public class SearchAssistiveDevice extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel createRentalPane = new CreateRentalPanel();
 
 	/**
 	 * Launch the application.
@@ -26,17 +34,172 @@ public class SearchAssistiveDevice extends JFrame {
 			}
 		});
 	}
+	
+	public void switchPanel(JPanel panel) {
+		contentPane.removeAll();
+		contentPane.add(panel);
+		contentPane.repaint();
+		contentPane.revalidate();
+	}
 
 	/**
 	 * Create the frame.
 	 */
-	public SearchAssistiveDevice() {
+	public SearchAssistiveDevice() {		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 920, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		Button button = new Button("S\u00F8g Hj\u00E6lpemiddel");
+		button.setBounds(27, 25, 119, 22);
+		contentPane.add(button);
+		
+		Button button_1 = new Button("S\u00F8g Beboer");
+		button_1.setBounds(152, 25, 115, 22);
+		contentPane.add(button_1);
+		
+		Button button_2 = new Button("Opret Udl\u00E5n");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				switchPanel(createRentalPane);
+			}
+		});
+		
+		button_2.setBounds(27, 53, 240, 22);
+		contentPane.add(button_2);
+		
+		TextField textField = new TextField();
+		textField.setBounds(27, 94, 292, 22);
+		contentPane.add(textField);
+		
+		Button button_3 = new Button("S\u00F8g");
+		button_3.setBounds(325, 94, 52, 22);
+		contentPane.add(button_3);
+		
+		List list = new List();
+		list.setBounds(27, 122, 350, 332);
+		contentPane.add(list);
+		
+		Label label = new Label("BEBOER");
+		label.setFont(new Font("Dialog", Font.PLAIN, 20));
+		label.setBounds(463, 53, 105, 22);
+		contentPane.add(label);
+		
+		Label label_1 = new Label("HJ\u00C6LPEMIDDEL");
+		label_1.setFont(new Font("Dialog", Font.PLAIN, 20));
+		label_1.setBounds(463, 250, 160, 22);
+		contentPane.add(label_1);
+		
+		Label label_2 = new Label("//TODO_LOGIN_INFO");
+		label_2.setBounds(740, 10, 133, 22);
+		contentPane.add(label_2);
+		
+		Label label_3 = new Label("NAVN");
+		label_3.setBounds(454, 291, 85, 22);
+		contentPane.add(label_3);
+		
+		Label label_4 = new Label("TYPE");
+		label_4.setBounds(454, 319, 75, 22);
+		contentPane.add(label_4);
+		
+		Label label_5 = new Label("STATUS");
+		label_5.setBounds(454, 347, 62, 22);
+		contentPane.add(label_5);
+		
+		Label label_6 = new Label("HMI");
+		label_6.setBounds(454, 375, 85, 22);
+		contentPane.add(label_6);
+		
+		Label label_7 = new Label("NAVN");
+		label_7.setBounds(595, 83, 62, 22);
+		contentPane.add(label_7);
+		
+		Label label_8 = new Label("CPR-NR");
+		label_8.setBounds(595, 111, 62, 22);
+		contentPane.add(label_8);
+		
+		Label label_9 = new Label("ADRESSE");
+		label_9.setBounds(595, 139, 62, 22);
+		contentPane.add(label_9);
+		
+		Label label_10 = new Label("BOLIG NUMMER");
+		label_10.setBounds(595, 167, 85, 22);
+		contentPane.add(label_10);
+		
+		Label label_11 = new Label("KOMMUNE");
+		label_11.setBounds(595, 195, 62, 22);
+		contentPane.add(label_11);
+		
+		Label label_12 = new Label("ID #");
+		label_12.setBounds(618, 224, 62, 22);
+		contentPane.add(label_12);
+		
+		Button button_4 = new Button("Tilf\u00F8j hj\u00E6lpemiddel");
+		button_4.setBounds(454, 432, 124, 22);
+		contentPane.add(button_4);
+		
+		Button button_5 = new Button("Annuller udlejning");
+		button_5.setBounds(596, 471, 115, 22);
+		contentPane.add(button_5);
+		
+		Button button_6 = new Button("Gem Udlejning");
+		button_6.setBounds(728, 471, 115, 22);
+		contentPane.add(button_6);
+		
+		TextField textField_1 = new TextField();
+		textField_1.setBounds(545, 291, 133, 22);
+		contentPane.add(textField_1);
+		
+		TextField textField_2 = new TextField();
+		textField_2.setBounds(545, 319, 133, 22);
+		contentPane.add(textField_2);
+		
+		TextField textField_4 = new TextField();
+		textField_4.setBounds(545, 347, 133, 22);
+		contentPane.add(textField_4);
+		
+		TextField textField_5 = new TextField();
+		textField_5.setBounds(545, 375, 133, 22);
+		contentPane.add(textField_5);
+		
+		TextField textField_6 = new TextField();
+		textField_6.setBounds(686, 167, 166, 22);
+		contentPane.add(textField_6);
+		
+		TextField textField_7 = new TextField();
+		textField_7.setBounds(686, 195, 166, 22);
+		contentPane.add(textField_7);
+		
+		TextField textField_8 = new TextField();
+		textField_8.setBounds(686, 83, 166, 22);
+		contentPane.add(textField_8);
+		
+		TextField textField_9 = new TextField();
+		textField_9.setBounds(686, 111, 166, 22);
+		contentPane.add(textField_9);
+		
+		TextField textField_10 = new TextField();
+		textField_10.setBounds(686, 139, 166, 22);
+		contentPane.add(textField_10);
+		
+		Label label_14 = new Label("EJER");
+		label_14.setBounds(454, 403, 62, 22);
+		contentPane.add(label_14);
+		
+		TextField textField_11 = new TextField();
+		textField_11.setBounds(545, 403, 133, 22);
+		contentPane.add(textField_11);
+		
+		TextArea textArea = new TextArea();
+		textArea.setBounds(684, 291, 200, 163);
+		contentPane.add(textArea);
+		
+		Label label_13 = new Label("NOTE");
+		label_13.setBounds(686, 266, 62, 22);
+		contentPane.add(label_13);
+		
 	}
-
 }
