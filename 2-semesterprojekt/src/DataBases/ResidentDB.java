c1b649e8d7a833179fcfc1c69cf7bffc86f1641d
WoahabQ <75674776+WoahabQ@users.noreply.github.com>
2021-05-18 11:30:42 +0200

    venstre side

--- 2-semesterprojekt/src/DataBases/ResidentDB.java
+++ 2-semesterprojekt/src/DataBases/ResidentDB.java
@@ -2,7 +2,11 @@ package DataBases;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import model.Resident;
+import modelLayer.Customer;
 import modelLayer.DBConnection;
 
 public class ResidentDB {
@@ -18,6 +22,41 @@ public class ResidentDB {
 
 		findCustomerByIdPS = con.prepareStatement(FIND_BY_ID);
 		findCustomerByEmailPS = con.prepareStatement(FIND_BY_EMAIL);
+
+	}
+	
+	public Resident findCustomerByEmail(String email) throws Exception {
+		Resident res = null;
+
+		try {
+			findCustomerByEmailPS.setString(1, email);
+			ResultSet rs = findCustomerByEmailPS.executeQuery();
+
+			if (rs.next()) {
+
+				res = buildResidentObject(rs);
+
+			}
+
+		} catch (SQLException s) {
+			s.printStackTrace();
+		}
+
+		return res;
+
+	}
+
+	private Resident buildResidentObject(ResultSet rs) {
+		Resident resident = null;
+
+		try {
+			resident = new Resident(rs.getString("fName"), rs.getString("lName"), rs.getString("email"), rs.getString("phoneNo"), rs.getString("streetName"), rs.getInt("streetNo"), rs.getInt("FKlocationId"));
+			resident.setId(rs.getInt("id"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return resident;
 
 	}
 
