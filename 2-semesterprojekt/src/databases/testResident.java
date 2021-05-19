96b906a3da4144d944cf530ccf6bc9dc23a97d7c
TheisTechWizard <75674693+TheisTechWizard@users.noreply.github.com>
2021-05-19 10:22:55 +0200

    IT WOOOORKS!!!

--- 2-semesterprojekt/src/databases/testResident.java
+++ 2-semesterprojekt/src/databases/testResident.java
@@ -8,16 +8,14 @@ public class testResident {
 	private static Resident resident;
 
 	public static Resident residentRun() throws Exception {
+		ResidentDB db = new ResidentDB();
 		return db.findCustomerByEmail("Felix@gmail.dk");
 	}
 
 	public static void main(String[] args) throws Exception {
 
-		ResidentDB db = new ResidentDB();
-
 		Resident a = residentRun();
 
 		System.out.println(a.getName());
 	}
-
 }
