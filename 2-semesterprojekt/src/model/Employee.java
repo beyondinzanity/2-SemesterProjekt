64f04003c908c0d757da020ddedf4c052acdfce8
TheisTechWizard <75674693+TheisTechWizard@users.noreply.github.com>
2021-05-20 13:09:35 +0200

    Data tingerlinger

--- 2-semesterprojekt/src/model/Employee.java
+++ 2-semesterprojekt/src/model/Employee.java
@@ -1,7 +1,11 @@
 package model;
 
 public class Employee {
-	
+
+	String ssn;
 	int employeeNumber;
+	String fname;
+	String lname;
+	String phonenumber;
 
 }
--- 2-semesterprojekt/src/model/Municipality.java
+++ 2-semesterprojekt/src/model/Municipality.java
@@ -1,8 +1,29 @@
 package model;
 
 public class Municipality {
-	
-	String name; 
+
+	String name;
 	String region;
+
+	public Municipality(String name, String region) {
+		this.name = name;
+		this.region = region;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
 
 }
