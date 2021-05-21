979d0b146005329d2cfcc21c01c087865c0e33f9
BeyonDInzanitY <nicklas@ostvand.dk>
2021-05-21 12:19:43 +0200

    Update DBConnection.java

--- 2-semesterprojekt/src/databases/DBConnection.java
+++ 2-semesterprojekt/src/databases/DBConnection.java
@@ -10,35 +10,30 @@ public class DBConnection {
 	private static DBConnection dbConnection;
 
 	private static final String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	private static final String dbName = "GuldBoSund";
-	private static final String serverAddress = "localhost";
+	private static final String dbName = "dmab0920_1086253";
+	private static final String serverAddress = "hildur.ucn.dk";
 	// private static final String serverAddress = "192.168.56.2";
 	private static final int serverPort = 1433;
-	private static final String userName = "sa";
-	private static final String password = "secret";
+	private static final String userName = "dmab0920_1086253";
+	private static final String password = "Password1!";
 
 	private DBConnection() {
-//		String connectionString = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s",
-//				serverAddress, serverPort, dbName, userName, password);
-		String connectionString2 = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s;",
-				serverAddress, serverPort, dbName, userName, password); // TEST MÅ GERNE SLETTES!
+		String connectionString = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s",
+				serverAddress, serverPort, dbName, userName, password);
+
 		try {
 			Class.forName(driverClass);
-			connection = DriverManager.getConnection(connectionString2); // TEST MÅ GERNE SLETTES!
 
-//			connection = DriverManager.getConnection(connectionString);
+			connection = DriverManager.getConnection(connectionString);
 		} catch (ClassNotFoundException e) {
 			System.err.println("Could not load JDBC driver");
 			e.printStackTrace();
 		} catch (SQLException e) {
-			e.printStackTrace(); // TEST MÅ GERNE SLETTES!
-			System.out.println("fejl SQL"); // TEST MÅ GERNE SLETTES!
-
-//			System.err.println("Could not connect to database " + dbName + "@" + serverAddress + ":" + serverPort
-//					+ " as user " + userName + " using password ******");
-//			System.out.println("Connection string was: "
-//					+ connectionString.substring(0, connectionString.length() - password.length()) + "....");
-//			e.printStackTrace();
+			System.err.println("Could not connect to database " + dbName + "@" + serverAddress + ":" + serverPort
+					+ " as user " + userName + " using password ******");
+			System.out.println("Connection string was: "
+					+ connectionString.substring(0, connectionString.length() - password.length()) + "....");
+			e.printStackTrace();
 		}
 	}
 
