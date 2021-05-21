31f68d3e2e0744ebd370bb03796a5067e0a615b1
BeyonDInzanitY <nicklas@ostvand.dk>
2021-05-21 10:08:53 +0200

    PULL THEIS

--- 2-semesterprojekt/src/databases/AssistiveDeviceDB.java
+++ 2-semesterprojekt/src/databases/AssistiveDeviceDB.java
@@ -1,6 +1,13 @@
 package databases;
 
+import model.AssistiveDevice;
 
 public class AssistiveDeviceDB implements IAssistiveDeviceDB {
+
+	@Override
+	public AssistiveDevice findAssistiveDevice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
--- 2-semesterprojekt/src/databases/AssistiveDeviceInstanceDB.java
+++ 2-semesterprojekt/src/databases/AssistiveDeviceInstanceDB.java
@@ -10,7 +10,7 @@ import model.Resident;
 
 public class AssistiveDeviceInstanceDB implements IAssistiveDeviceInstanceDB {
 
-	private static final String FIND_BY_DEVICE_ID = "select * from AssistiveDeviceId where id = ?";
+	private static final String FIND_BY_DEVICE_ID = "select * from AssistiveDeviceInstance where id = ?";
 	private PreparedStatement findAssistiveDeviceInstanceByDeviceIdPS;
 
 	public AssistiveDeviceInstanceDB() throws DataAccessException, SQLException {
@@ -20,36 +20,36 @@ public class AssistiveDeviceInstanceDB implements IAssistiveDeviceInstanceDB {
 		findAssistiveDeviceInstanceByDeviceIdPS = con.prepareStatement(FIND_BY_DEVICE_ID);
 
 	}
-	
+
 	@Override
 	public AssistiveDeviceInstance findInstanceByDeviceId(int deviceId) {
 		// TODO Auto-generated method stub
 		AssistiveDeviceInstance res = null;
-		
+
 		try {
 			findAssistiveDeviceInstanceByDeviceIdPS.setInt(1, deviceId);
 			ResultSet rs = findAssistiveDeviceInstanceByDeviceIdPS.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				res = buildAssistiveDeviceInstanceObject(rs);
 				System.out.println(res);
 			}
-		} catch (SQLException s){
+		} catch (SQLException s) {
 			s.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	private AssistiveDeviceInstance buildAssistiveDeviceInstanceObject(ResultSet rs) {
 		AssistiveDeviceInstance assistiveDeviceInstance = null;
-		
+
 		try {
-			assistiveDeviceInstance = new AssistiveDeviceInstance(rs.getString("barcode"), rs.getDate("registeredDate"), rs.getString("note"));
+			assistiveDeviceInstance = new AssistiveDeviceInstance(rs.getString("barcode"), rs.getDate("registeredDate"),
+					rs.getString("note"));
 		} catch (SQLException s) {
 			s.printStackTrace();
 		}
 		return assistiveDeviceInstance;
 	}
-	
 
 }
